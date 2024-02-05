64916a54c086b79e9278cc8281f8da522568b05e
Kirill <kirill.streltsov0@yandex.ru>
2024-02-05 21:34:32 +0300

    fix: user can not get warning twice

--- src/main/java/listeners/MessageListener.java
+++ src/main/java/listeners/MessageListener.java
@@ -6,11 +6,11 @@ import db.UserDbHelper;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class MessageListener extends ListenerAdapter {
@@ -33,38 +33,38 @@ public class MessageListener extends ListenerAdapter {
         List<String> bannedWords = bannedWordsDbHelper.findBannedWordsByServerId(guild.getId());
         List<String> ignoredChats = ignoredChatsDbHelper.findIgnoredChatsByServerId(guild.getId());
 
-        if(ignoredChats.contains(event.getGuildChannel().asTextChannel().getId())) return;
+        if (ignoredChats.contains(event.getGuildChannel().asTextChannel().getId())) return;
 
         String content = event.getMessage().getContentRaw().toLowerCase();
 
-        bannedWords.forEach(w ->{
-            if(content.contains(w.toLowerCase())) {
-                int warnings = userDbHelper.getWarnings(guild.getId(), member.getUser().getId());
-                MessageCreateAction messageCreateAction = event
-                        .getGuildChannel()
+        Optional<String> foundWord = bannedWords.stream().filter(content::contains).findFirst();
+
+        if (!foundWord.isPresent()) return;
+
+        int warnings = userDbHelper.getWarnings(guild.getId(), member.getUser().getId());
+        MessageCreateAction messageCreateAction = event
+                .getGuildChannel()
+                .asTextChannel()
+                .sendMessage("Pizda tebe " + author.getEffectiveName());
+
+        if (warnings >= 2) {
+            if (!member.isOwner()) {
+                event.getGuild()
+                        .ban(UserSnowflake.fromId(member.getUser().getId()), 1, TimeUnit.DAYS)
+                        .reason("Swear words usage")
+                        .queue();
+
+                event.getGuildChannel()
                         .asTextChannel()
-                        .sendMessage("Pizda tebe " + author.getEffectiveName());
-
-                if (warnings >= 2) {
-                    if (!member.isOwner()) {
-                        event.getGuild()
-                                .ban(UserSnowflake.fromId(member.getUser().getId()), 1, TimeUnit.DAYS)
-                                .reason("Swear words usage")
-                                .queue();
-
-                        event.getGuildChannel()
-                                .asTextChannel()
-                                .sendMessage(member.getEffectiveName() + " was banned for 1 day. Pizda emu")
-                                .queue();
-                        return;
-                    }
-
-                    userDbHelper.updateWarnings(guild.getId(), member.getUser().getId(), 0);
-                }
-
-                userDbHelper.updateWarnings(guild.getId(), member.getUser().getId(), warnings + 1);
-                messageCreateAction.queue();
+                        .sendMessage(member.getEffectiveName() + " was banned for 1 day. Pizda emu")
+                        .queue();
+                return;
             }
-        });
+
+            userDbHelper.updateWarnings(guild.getId(), member.getUser().getId(), 0);
+        }
+
+        userDbHelper.updateWarnings(guild.getId(), member.getUser().getId(), warnings + 1);
+        messageCreateAction.queue();
     }
 }
